805e32a3f156024d80ea40c9e8ee5fa6b769ee8f
Elle Young <missanyayoung@outlook.com>
2020-06-07 20:04:26 -0700

    update to security log, help feature needs updates, find command updated

--- security_check.sh
+++ security_check.sh
@@ -3,26 +3,44 @@
 # Title: security_check.sh
 # Date: 06/07/2020
 # Purpose: Generate a report of all files and folders with allow Read, Write and Execute for Other.
-# Update: Add stretch task for log variable.
+# Update: Add stretch task for log variable, add help/usage statement
 
 # Define a variable for date
 now=`date`
 
+# Define a log variable 
+sclog=security_check.log
+
 # Create a header at the top of the security log
 
-echo "*******************************" >> security_check.log
+echo "*******************************" >> $sclog
 
 # add hostname to the security log
-echo "*** Security report for: $HOSTNAME " >> security_check.log
+echo "*** Security report for: $HOSTNAME " >> $sclog
 
 # add date to the security log
-echo "*** Report Date: $now " >> security_check.log
+echo "*** Report Date: $now " >> $sclog
 
-# Add find command here and redirect the output to security_check.log
+# Add find command here and redirect the output to $sclog
+find / -type d -perm 777 >> $sclog
 
 # add two blank lines
-echo >> security_check.log
-echo >> security_check.log
+echo >> $sclog
+echo >> $sclog
 # close log for this run
-echo "**** End security report ******" >> security_check.log
-echo "*******************************" >> security_check.log
+echo "**** End security report ******" >> $sclog
+echo "*******************************" >> $sclog
+
+#help/usage statement
+#if [ "$1" = "-h"]
+ #then
+  #echo "This script will search your local system for any directories that grant Other full rights."
+  #echo usage: $0 [-h]
+  #exit 0
+#fi
+
+### variables
+sclog=security_check.log
+
+# Create a header at the top of the security log
+echo "*******************************" >> ${sc-log}
